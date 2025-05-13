8dfc491fbbabc37ffef3961a87fad0a4c79d97ff
sabi-hawk <miansabby516@gmail.com>
2025-05-13 10:27:24 +0500

    misc

--- src/App.js
+++ src/App.js
@@ -1,34 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import './App.css';
 
 function App() {
   const [storyIds, setStoryIds] = useState([]);
   const [titles, setTitles] = useState([]);
+
   useEffect(() => {
-    const fetchStories = async() => {
-      try {
-        const response = await axios.get('https://hacker-news.firebaseio.com/v0/topstories.json')
-        setStoryIds(response.data);
-
-
-        const titlePromises = response.data.map((id) =>
-          axios.get(`https://hacker-news.firebaseio.com/v0/item/${id}.json`)
-        );
-
-        const stories = await Promise.all(titlePromises);
-        const storyTitles = stories.map((res) => res.data?.title || 'No Title');
-        setTitles(storyTitles);
-        console.log(response.data)
-      } catch (err) {
-        console.log("Error")
-      }
-    }
+    
 
     fetchStories();
   },[]);
-
 
   return (
     <div className="App">
