27f9fe2c7ea5296863f913d9f03695ea7c071a8b
sabi-hawk <miansabby516@gmail.com>
2025-05-13 10:27:37 +0500

    misc

--- src/App.js
+++ src/App.js
@@ -7,7 +7,24 @@ function App() {
   const [titles, setTitles] = useState([]);
 
   useEffect(() => {
-    
+    const fetchStories = async() => {
+      try {
+        const response = await axios.get('https://hacker-news.firebaseio.com/v0/topstories.json')
+        setStoryIds(response.data);
+
+
+        const titlePromises = response.data.map((id) =>
+          axios.get(`https://hacker-news.firebaseio.com/v0/item/${id}.json`)
+        );
+
+        const stories = await Promise.all(titlePromises);
+        const storyTitles = stories.map((res) => res.data?.title || 'No Title');
+        setTitles(storyTitles);
+        console.log(response.data)
+      } catch (err) {
+        console.log("Error")
+      }
+    }
 
     fetchStories();
   },[]);
